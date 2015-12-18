9a8b7a3f830a578e955c93ad8c0de811f21f68a4
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-12-18 10:47:19 -0800

    Wrote visualize method

--- lib/router-visualizer/mapper.rb
+++ lib/router-visualizer/mapper.rb
@@ -4,8 +4,22 @@ module ActionDispatch
   module Routing
     class Mapper
       def visualize(options = {})
+        groups = options.delete(:groups) { ['development'] }
+        at = options.delete(:at) { '/routes' }
 
+        if matching_group? groups
+          mount RouterVisualizer::Engine, at: at
+        end
+        self
       end
+
+      private
+        def matching_group?(groups)
+          groups.each do |group|
+            return true if Rails.env.send("#{group}?")
+          end
+          false
+        end
     end
   end
 end
